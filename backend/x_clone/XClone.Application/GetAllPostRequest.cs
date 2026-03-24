08714a1e15e6fa6d58745ddac5fc0add8cd20bd8
Mora <morabryan322@gmail.com>
2026-03-24 12:28:35 -0500

    feat: proyecto backend x_clone

---
+++ backend/x_clone/XClone.Application/GetAllPostRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace XClone.Application
+{
+    public class GetAllPostRequest
+    {
+        public int? Limit { get; set; }
+
+        public int? Offset { get; set; }
+
+        public string? GitlabProfile { get; set; }
+    }
+}
---
+++ backend/x_clone/X_Clone.WebApi/Controllers/PostsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using XClone.Application;
+
+namespace XClone.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+
+        //Crear
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreatePostRequest model)
+        {
+            return Ok($"Post subido: autor: {model.AutorId}, contenido: {model.Texto}");
+        }
+
+        //obtener todos los post
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] GetAllPostRequest model)
+        {
+            return Ok($"Todos los usuarios: limit: {model.Limit}, outset: {model.Offset}, gitlabprofile: {model.GitlabProfile}");
+        }
+
+        //obtener un post
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            return Ok($"{id}");
+        }
+
+        //Actualizar
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> Update([FromBody] UpdatePostRequest model)
+        {
+            return Ok($"usuario actualizado: Contenido: {model.Texto}");
+        }
+
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return Ok("usuario eliminado");
+        }
+    }
+}
