d79bd7fa72e69980384e1c61e8755fab3eeb18cb
Mora <morabryan322@gmail.com>
2026-03-30 12:33:59 -0500

    feat: practica en clase dbcontext

---
+++ backend/PracticaDbContext/PracticaDbContext.Domain/DataBase/SqlServer/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PracticaDbContext.Domain.Database.SqlServer.Entities;
+
+public partial class Country
+{
+    public Guid Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public virtual ICollection<City> Cities { get; set; } = new List<City>();
+}
---
+++ backend/PracticaDbContext/PracticaDbContext.Domain/DataBase/SqlServer/Entities/Like.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PracticaDbContext.Domain.Database.SqlServer.Entities;
+
+public partial class Like
+{
+    public Guid Id { get; set; }
+
+    public Guid PostId { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public DateTime FechaCreacion { get; set; }
+
+    public virtual Post Post { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
