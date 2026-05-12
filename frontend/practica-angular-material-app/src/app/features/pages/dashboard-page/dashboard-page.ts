afa7f95fdbef27717f6e3bbc31e94ac56dd3aed2
Mora <morabryan322@gmail.com>
2026-05-11 23:08:46 -0500

    feat: actualizar header con logos y direcciones

--- frontend/practica-angular-material-app/src/app/features/pages/dashboard-page/dashboard-page.ts
+++ frontend/practica-angular-material-app/src/app/features/pages/dashboard-page/dashboard-page.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref, RouterLink } from '@angular/router';
 import { Navbar } from '../../../shared/navbar/navbar';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [RouterOutlet, Navbar, MatIconModule],
+  imports: [RouterOutlet, Navbar, MatIconModule, RouterLinkWithHref, RouterLink],
   templateUrl: './dashboard-page.html',
   styleUrl: './dashboard-page.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
